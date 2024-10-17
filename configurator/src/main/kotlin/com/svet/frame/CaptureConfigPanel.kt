0a4e900c9cf1b33169ee048b4bd86a75e83f1e90
Sirkin <isirkin@vtb.ru>
2024-10-17 21:53:05 +0300

    - code refactoring

--- configurator/src/main/kotlin/com/svet/frame/CaptureConfigPanel.kt
+++ configurator/src/main/kotlin/com/svet/frame/CaptureConfigPanel.kt
@@ -51,8 +51,10 @@ class CaptureConfigPanel internal constructor() : JPanel(), ActionListener {
     }
 
     private fun drawGridLines(g2D: Graphics2D) {
-        g2D.drawLine(svetConfig.captureConfig.width / 2, 0, svetConfig.captureConfig.width / 2, svetConfig.captureConfig.height)
-        g2D.drawLine(0, svetConfig.captureConfig.height / 2, svetConfig.captureConfig.width, svetConfig.captureConfig.height / 2)
+        val x = svetConfig.captureConfig.width / 2
+        val y = svetConfig.captureConfig.height / 2
+        g2D.drawLine(x, 0, x, svetConfig.captureConfig.height)
+        g2D.drawLine(0, y, svetConfig.captureConfig.width, y)
     }
 
     private fun drawCaptureColoredRegion(
@@ -163,152 +165,6 @@ class CaptureConfigPanel internal constructor() : JPanel(), ActionListener {
             g2D, colors, offset, svetConfig.captureConfig.bottomCountR, 0, oy * (-1),
             vLineX, svetConfig.captureConfig.border * (-1), vLineX, svetConfig.captureConfig.section * (-1)
         )
-    }
-
-    private fun drawCaptureRegionsColor(g2D: Graphics2D) {
-
-        val colors = captureScreen.getRegionsCaptureColors(captureScreen.capture(), svetConfig.captureConfig)
-
-        g2D.stroke = BasicStroke(svetConfig.captureConfig.border.toFloat())
-
-        // области захвата снизу слева
-        var offset = 0
-        for (i in 0 until svetConfig.captureConfig.bottomCountL) {
-
-            val x = svetConfig.captureConfig.positions[i].x + svetConfig.captureConfig.captureRegionWidth / 2
-            val y = svetConfig.captureConfig.height - svetConfig.captureConfig.captureRegionHeight
-            g2D.drawLine(x, y - svetConfig.captureConfig.border, x, y - svetConfig.captureConfig.section - svetConfig.captureConfig.border)
-
-            graphics.drawRect(svetConfig.captureConfig.positions[i].x, svetConfig.captureConfig.positions[i].y, svetConfig.captureConfig.captureRegionWidth + svetConfig.captureConfig.border,svetConfig.captureConfig.captureRegionHeight + svetConfig.captureConfig.border)
-
-            g2D.drawRect(
-                svetConfig.captureConfig.positions[i].x - svetConfig.captureConfig.border,
-                svetConfig.captureConfig.positions[i].y - svetConfig.captureConfig.captureRegionHeight - svetConfig.captureConfig.section - svetConfig.captureConfig.border * 2,
-                svetConfig.captureConfig.captureRegionWidth + svetConfig.captureConfig.border,
-                svetConfig.captureConfig.captureRegionHeight + svetConfig.captureConfig.border
-            )
-
-            g2D.drawImage(
-                ImageProcessorUtils.createMonotonousImage(
-                    svetConfig.captureConfig.captureRegionWidth,
-                    svetConfig.captureConfig.captureRegionHeight,
-                    BufferedImage.TYPE_3BYTE_BGR,
-                    colors[i]
-                ),
-                svetConfig.captureConfig.positions[i].x,
-                svetConfig.captureConfig.positions[i].y - svetConfig.captureConfig.captureRegionHeight - svetConfig.captureConfig.section - svetConfig.captureConfig.border,
-                this
-            )
-        }
-
-        // области захвата слева
-        offset += svetConfig.captureConfig.bottomCountL
-        for (i in 0 until svetConfig.captureConfig.leftCount) {
-
-            val x = svetConfig.captureConfig.captureRegionWidth
-            val y = svetConfig.captureConfig.positions[i + offset].y + svetConfig.captureConfig.captureRegionHeight / 2
-            g2D.drawLine(x, y, x + svetConfig.captureConfig.section, y)
-
-            g2D.drawRect(
-                svetConfig.captureConfig.positions[i + offset].x - svetConfig.captureConfig.border + svetConfig.captureConfig.captureRegionWidth + svetConfig.captureConfig.section + svetConfig.captureConfig.border * 2,
-                svetConfig.captureConfig.positions[i + offset].y - svetConfig.captureConfig.border,
-                svetConfig.captureConfig.captureRegionWidth + svetConfig.captureConfig.border,
-                svetConfig.captureConfig.captureRegionHeight + svetConfig.captureConfig.border
-            )
-        }
-
-        // области захвата сверху
-        offset += svetConfig.captureConfig.leftCount
-        for (i in 0 until svetConfig.captureConfig.topCount) {
-
-            val x = svetConfig.captureConfig.positions[i + offset].x + svetConfig.captureConfig.captureRegionWidth / 2
-            val y = svetConfig.captureConfig.captureRegionHeight
-            g2D.drawLine(x, y + svetConfig.captureConfig.border, x, y + svetConfig.captureConfig.section + svetConfig.captureConfig.border)
-
-            g2D.drawRect(
-                svetConfig.captureConfig.positions[i + offset].x - svetConfig.captureConfig.border,
-                svetConfig.captureConfig.positions[i + offset].y - svetConfig.captureConfig.border + svetConfig.captureConfig.captureRegionHeight + svetConfig.captureConfig.section + svetConfig.captureConfig.border * 2,
-                svetConfig.captureConfig.captureRegionWidth + svetConfig.captureConfig.border,
-                svetConfig.captureConfig.captureRegionHeight + svetConfig.captureConfig.border
-            )
-        }
-
-        // области захвата справа
-        offset += svetConfig.captureConfig.topCount
-        for (i in 0 until svetConfig.captureConfig.leftCount) {
-
-            val x = svetConfig.captureConfig.width - svetConfig.captureConfig.captureRegionWidth
-            val y = svetConfig.captureConfig.positions[i + offset].y + svetConfig.captureConfig.captureRegionHeight / 2
-            g2D.drawLine(x, y, x - svetConfig.captureConfig.section, y)
-
-            g2D.drawRect(
-                svetConfig.captureConfig.positions[i + offset].x - svetConfig.captureConfig.border - svetConfig.captureConfig.captureRegionWidth - svetConfig.captureConfig.section - svetConfig.captureConfig.border * 2,
-                svetConfig.captureConfig.positions[i + offset].y - svetConfig.captureConfig.border,
-                svetConfig.captureConfig.captureRegionWidth + svetConfig.captureConfig.border,
-                svetConfig.captureConfig.captureRegionHeight + svetConfig.captureConfig.border
-            )
-        }
-
-        // области захвата снизу справа
-        offset += svetConfig.captureConfig.rightCount
-        for (i in 0 until svetConfig.captureConfig.bottomCountR) {
-
-            val x = svetConfig.captureConfig.positions[i+ offset].x + svetConfig.captureConfig.captureRegionWidth / 2
-            val y = svetConfig.captureConfig.height - svetConfig.captureConfig.captureRegionHeight
-            g2D.drawLine(x, y - svetConfig.captureConfig.border, x, y - svetConfig.captureConfig.section - svetConfig.captureConfig.border)
-
-            g2D.drawRect(
-                svetConfig.captureConfig.positions[i + offset].x - svetConfig.captureConfig.border,
-                svetConfig.captureConfig.positions[i + offset].y - svetConfig.captureConfig.border  - svetConfig.captureConfig.captureRegionWidth - svetConfig.captureConfig.section - svetConfig.captureConfig.border * 2,
-                svetConfig.captureConfig.captureRegionWidth + svetConfig.captureConfig.border,
-                svetConfig.captureConfig.captureRegionHeight + svetConfig.captureConfig.border
-            )
-        }
-    }
-
-    private fun drawCaptureRegions(graphics: Graphics) {
-        val captureRegion = ImageProcessorUtils.createMonotonousImage(
-            svetConfig.captureConfig.captureRegionWidth,
-            svetConfig.captureConfig.captureRegionHeight,
-            BufferedImage.TYPE_3BYTE_BGR,
-            Color.GREEN
-        )
-
-        for (i in svetConfig.captureConfig.positions.indices) {
-            graphics.drawImage(captureRegion, svetConfig.captureConfig.positions[i].x, svetConfig.captureConfig.positions[i].y, this)
-
-            val regionNumStr = i.toString()
-            graphics.drawString(
-                regionNumStr,
-                svetConfig.captureConfig.positions[i].x + svetConfig.captureConfig.captureRegionWidth / 2 - graphics.fontMetrics.stringWidth(regionNumStr) / 2,
-                svetConfig.captureConfig.positions[i].y + svetConfig.captureConfig.captureRegionHeight / 2 + verticalFontAlignCorrection
-            )
-        }
-    }
-
-    private fun drawCaptureRegionsAvgColors(graphics: Graphics) {
-        val colors = captureScreen.getRegionsCaptureColors(captureScreen.capture(), svetConfig.captureConfig)
-
-        for (i in svetConfig.captureConfig.positions.indices) {
-            graphics.drawImage(
-                ImageProcessorUtils.createMonotonousImage(
-                    svetConfig.captureConfig.captureRegionWidth,
-                    svetConfig.captureConfig.captureRegionHeight,
-                    BufferedImage.TYPE_3BYTE_BGR,
-                    colors[i]
-                ),
-                svetConfig.captureConfig.positions[i].x,
-                svetConfig.captureConfig.positions[i].y,
-                this
-            )
-
-            val regionNumStr = i.toString()
-            graphics.drawString(
-                regionNumStr,
-                svetConfig.captureConfig.positions[i].x + svetConfig.captureConfig.captureRegionWidth / 2 - graphics.fontMetrics.stringWidth(regionNumStr) / 2,
-                svetConfig.captureConfig.positions[i].y + svetConfig.captureConfig.captureRegionHeight / 2 + verticalFontAlignCorrection
-            )
-        }
     }
 
 }
